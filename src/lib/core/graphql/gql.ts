cd71f4ef950bb4afbecf8026ca8adfb8ed4fe18b
Sunny Pelletier <sunny.pelletier@qohash.com>
2021-09-18 15:25:20 -0400

    add minify and maxify requests

--- src/lib/core/graphql/gql.ts
+++ src/lib/core/graphql/gql.ts
@@ -1,5 +1,15 @@
+import { format } from 'graphql-formatter'
+import compress from 'graphql-query-compress'
 import { gql as implementation } from 'graphql-request'
 
 export default function (chunks: TemplateStringsArray): string {
   return implementation(chunks)
+}
+
+export function prettify(request: string): string {
+  return format(request)
+}
+
+export function minify(request: string): string {
+  return compress(request)
 }
