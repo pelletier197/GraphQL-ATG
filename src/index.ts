3dac07bcdfb8c42c5898419defc74af28fcadcb4
Sunny Pelletier <sunny.pelletier@qohash.com>
2021-10-09 11:28:20 -0400

    add task management base

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,101 @@
-import { runGraphQLAtg } from '@lib/core/atg/graphqlAtg'
-import { getAtgConfiguration } from '@lib/core/cli/cli'
-import { failed } from '@lib/core/progress/progressIndicator'
+/* eslint-disable @typescript-eslint/no-unused-vars */
+// import { runGraphQLAtg } from '@lib/core/atg/graphqlAtg'
+// import { getAtgConfiguration } from '@lib/core/cli/cli'
+import { newMultiTask, newTask } from '@lib/core/progress/progressIndicator'
 
-async function run() {
-  const config = await getAtgConfiguration()
+// async function run() {
+//   const config = await getAtgConfiguration()
+
+//   try {
+//     await runGraphQLAtg(config)
+//   } catch (error) {
+//     failed()
+
+//     throw error
+//   }
+// }
+
+async function yeah() {
+  const task = newTask(
+    () => {
+      return new Promise((_, reject) => {
+        setTimeout(() => reject('potato'), 2000)
+      })
+    },
+    {
+      exitOnError: true,
+      name: 'YEAH',
+    }
+  )
 
   try {
-    await runGraphQLAtg(config)
+    const result = await task.start()
+    console.log(result)
   } catch (error) {
-    failed()
+    console.log('error', error)
+  }
 
-    throw error
+  const multiTask = newMultiTask(
+    [
+      {
+        name: 'Test 1',
+        run: () => {
+          return new Promise((_, reject) => {
+            setTimeout(() => reject('potato'), 1000)
+          })
+        },
+      },
+      {
+        name: 'Test 2',
+        run: () => {
+          return new Promise((resolve) => {
+            setTimeout(() => resolve('potato'), 4000)
+          })
+        },
+      },
+      {
+        name: 'Test 3',
+        run: (context) => {
+          return new Promise((resolve) => {
+            setTimeout(() => {
+              context.updateName('New test 3 homies')
+              setTimeout(() => resolve('YEAH'), 3000)
+            }, 4000)
+          })
+        },
+      },
+      {
+        name: 'Test 4',
+        run: () => {
+          return new Promise((_, reject) => {
+            setTimeout(() => reject('no internet'), 50)
+          })
+        },
+      },
+      {
+        name: 'Test 5',
+        run: () => {
+          return new Promise((resolve) => {
+            setTimeout(() => resolve('oh now you have internet'), 5000)
+          })
+        },
+      },
+    ],
+    {
+      exitOnError: true,
+      name: 'Super multi-tasking',
+      concurrency: 2,
+    }
+  )
+
+  try {
+    const result = await multiTask.start()
+    console.log(result)
+  } catch (error) {
+    console.log('error', error)
   }
 }
 
-run()
+yeah()
+
+// run()
--- src/lib/core/progress/progressIndicator.ts
+++ src/lib/core/progress/progressIndicator.ts
@@ -1,3 +1,6 @@
+/* eslint-disable functional/immutable-data */
+import { Semaphore } from 'await-semaphore'
+import { Listr } from 'listr2'
 import ora from 'ora'
 
 const spinner = ora({
@@ -27,4 +30,105 @@ export function failed(reason?: string) {
   } else {
     spinner.fail(reason)
   }
+}
+
+export function newTask<T>(
+  task: (context: TaskContext) => Promise<T>,
+  config: TaskConfig
+): Task<T> {
+  const tasks = new Listr(
+    {
+      title: config.name,
+      task: async (context, wrapper) => {
+        const result = await task({
+          updateName: (newName: string) => {
+            wrapper.title = newName
+            return newName
+          },
+        })
+
+        context.result = result
+      },
+    },
+    {
+      exitOnError: config.exitOnError,
+    }
+  )
+
+  return {
+    start: async () => (await tasks.run()).result,
+  }
+}
+
+export function newMultiTask<T>(
+  subTasks: ReadonlyArray<SubTask<T>>,
+  config: MultiTaskConfig
+): Task<ReadonlyArray<T>> {
+  const semaphore = new Semaphore(Math.max(config.concurrency, 1))
+
+  const tasks = new Listr(
+    {
+      title: config.name,
+      task: async (context, wrapper) => {
+        context.results = []
+
+        wrapper.newListr(
+          subTasks.map((subTask: SubTask<T>) => {
+            return {
+              title: 'Pending...',
+              task: async (subContext, subWrapper) => {
+                const release = await semaphore.acquire()
+
+                subWrapper.title = subTask.name
+
+                try {
+                  const result = await subTask.run({
+                    updateName: (newName: string) => {
+                      subWrapper.title = newName
+                      return newName
+                    },
+                  })
+
+                  subContext.results.push(result)
+                } finally {
+                  release()
+                }
+              },
+            }
+          })
+        )
+      },
+    },
+    {
+      exitOnError: config.exitOnError,
+      concurrent: true,
+    }
+  )
+
+  return {
+    start: async () => (await tasks.run()).results,
+  }
+}
+
+// eslint-disable-next-line functional/no-mixed-type
+export type SubTask<T> = {
+  readonly name: string
+  readonly run: (context: TaskContext) => Promise<T>
+}
+
+export type TaskConfig = {
+  readonly name: string
+  readonly exitOnError: boolean
+}
+
+export type MultiTaskConfig = TaskConfig & {
+  readonly concurrency: number
+}
+
+export type TaskContext = {
+  readonly updateName: (newName: string) => string
+}
+
+export type Task<T> = {
+  readonly start: () => Promise<T>
 }
