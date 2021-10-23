d6191186085e7f80b7a72d55cb37ac77390c8027
Sunny Pelletier <sunny.pelletier@qohash.com>
2021-10-23 15:51:22 -0400

    use test in CI and publish with build

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
+#!/usr/bin/env node
+
 import { runGraphQLAtg } from '@lib/core/atg/graphqlAtg.js'
 import { getAtgConfiguration } from '@lib/core/cli/cli.js'
+
+function verifyVersion() {
+  const version = process.version
+  console.log(`Running on NodeJS version ${version}`)
+}
 
 async function run() {
   const config = await getAtgConfiguration()
@@ -7,4 +14,5 @@ async function run() {
   process.exit(results.failed)
 }
 
+verifyVersion()
 run()
