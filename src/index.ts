792dea027550ac79b18037b0ffd5d9361f191b3c
Sunny Pelletier <sunny.pelletier@qohash.com>
2021-10-24 09:27:07 -0400

    update min version

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,10 @@
 import { runGraphQLAtg } from '@lib/core/atg/graphqlAtg.js'
 import { getAtgConfiguration } from '@lib/core/cli/cli.js'
 
-function verifyVersion() {
-  const version = process.version
-  console.log(`Running on NodeJS version ${version}`)
-}
-
 async function run() {
   const config = await getAtgConfiguration()
   const results = await runGraphQLAtg(config)
   process.exit(results.failed)
 }
 
-verifyVersion()
 run()
